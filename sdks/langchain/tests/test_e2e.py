4f5028eb87f81e601ab865e1770da3228804575b
Twisha Bansal <twishabansal@google.com>
2025-01-07 11:41:45 +0530

    without auth tests

--- sdks/langchain/tests/test_e2e.py
+++ sdks/langchain/tests/test_e2e.py
@@ -10,53 +10,36 @@ from toolbox_langchain_sdk.client import ToolboxClient
 class TestE2EClient:
     @pytest_asyncio.fixture(scope="function")
     async def toolbox(self):
-        toolbox = ToolboxClient("https://toolbox-1025562370644.us-central1.run.app")
+        toolbox = ToolboxClient("http://localhost:5000")
         yield toolbox
         await toolbox.close()
 
-    # @pytest.mark.asyncio
-    # async def test_load_tool(self, toolbox):
-    #     tool = await toolbox.load_tool("get-n-rows")
-    #     response = await tool.arun({"num_rows": "2"})
-    #     result = response["result"]
-
-    #     assert "test text 1" in result
-    #     assert "test text 2" in result
-    #     assert "test text 3" not in result
-
-    # @pytest.mark.asyncio
-    # async def test_load_toolset_all(self, toolbox):
-    #     toolset = await toolbox.load_toolset()
-    #     assert len(toolset) == 2
-    #     tool_names = ["get-n-rows", "get-row-by-id"]
-    #     assert toolset[0].name in tool_names
-    #     assert toolset[1].name in tool_names
-
-    # @pytest.mark.asyncio
-    # async def test_load_toolset_single(self, toolbox):
-    #     toolset = await toolbox.load_toolset("my-toolset")
-    #     assert len(toolset) == 1
-    #     assert toolset[0].name == "get-row-by-id"
-
-    #     toolset = await toolbox.load_toolset("my-toolset-2")
-    #     assert len(toolset) == 2
-    #     tool_names = ["get-n-rows", "get-row-by-id"]
-    #     assert toolset[0].name in tool_names
-    #     assert toolset[1].name in tool_names
-
     @pytest.mark.asyncio
-    async def test_load_tool_auth(self, toolbox):
-        # toolbox.add_auth_header("my-test-auth", get_id_token)
-        tool = await toolbox.load_tool(
-            "get-row-by-id-auth",
-            auth_tokens={"my-test-auth": get_id_token}
-        )
-        print("Tool definition", tool)
-        response = await tool.arun({})
-
+    async def test_load_tool(self, toolbox):
+        tool = await toolbox.load_tool("get-n-rows")
+        response = await tool.arun({"num_rows": "2"})
         result = response["result"]
 
         assert "row1" in result
         assert "row2" in result
         assert "row3" not in result
 
+    @pytest.mark.asyncio
+    async def test_load_toolset_all(self, toolbox):
+        toolset = await toolbox.load_toolset()
+        assert len(toolset) == 2
+        tool_names = ["get-n-rows", "get-row-by-id"]
+        assert toolset[0].name in tool_names
+        assert toolset[1].name in tool_names
+
+    @pytest.mark.asyncio
+    async def test_load_toolset_single(self, toolbox):
+        toolset = await toolbox.load_toolset("my-toolset")
+        assert len(toolset) == 1
+        assert toolset[0].name == "get-row-by-id"
+
+        toolset = await toolbox.load_toolset("my-toolset-2")
+        assert len(toolset) == 2
+        tool_names = ["get-n-rows", "get-row-by-id"]
+        assert toolset[0].name in tool_names
+        assert toolset[1].name in tool_names
