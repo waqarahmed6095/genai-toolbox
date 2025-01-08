ac711ace9fc7891b6f1857ea5a8613fe91171d7d
Twisha Bansal <twishabansal@google.com>
2025-01-08 16:08:31 +0530

    File cleanup

--- sdks/langchain/tests/test_e2e.py
+++ sdks/langchain/tests/test_e2e.py
@@ -1,4 +1,3 @@
-import conftest as conftest
 import pytest
 import pytest_asyncio
 from aiohttp import ClientResponseError
@@ -26,21 +25,7 @@ class TestE2EClient:
         assert "row3" not in result
 
     @pytest.mark.asyncio
-    async def test_load_toolset_all(self, toolbox):
-        toolset = await toolbox.load_toolset()
-        assert len(toolset) == 5
-        tool_names = [
-            "get-n-rows",
-            "get-row-by-id",
-            "get-row-by-id-auth",
-            "get-row-by-email-auth",
-            "get-row-by-content-auth",
-        ]
-        for tool in toolset:
-            assert tool.name in tool_names
-
-    @pytest.mark.asyncio
-    async def test_load_toolset_single(self, toolbox):
+    async def test_load_toolset_specific(self, toolbox):
         toolset = await toolbox.load_toolset("my-toolset")
         assert len(toolset) == 1
         assert toolset[0].name == "get-row-by-id"
@@ -52,16 +37,21 @@ class TestE2EClient:
         assert toolset[1].name in tool_names
 
     @pytest.mark.asyncio
-    async def test_run_tool1(self, toolbox, auth_token2):
-        tool = await toolbox.load_tool(
+    async def test_load_toolset_all(self, toolbox):
+        toolset = await toolbox.load_toolset()
+        assert len(toolset) == 5
+        tool_names = [
+            "get-n-rows",
             "get-row-by-id",
-        )
-        toolbox.add_auth_token("my-test-auth", lambda: auth_token2)
-        response = await tool.arun({"id": "2"})
-        assert "row2" in response["result"]
+            "get-row-by-id-auth",
+            "get-row-by-email-auth",
+            "get-row-by-content-auth",
+        ]
+        assert {tool.name for tool in toolset} == tool_names
 
+    # If a tool requires no auth but auth tokens are passed, then they are ignored
     @pytest.mark.asyncio
-    async def test_run_tool2(self, toolbox, auth_token2):
+    async def test_run_tool_unauth_with_auth(self, toolbox, auth_token2):
         tool = await toolbox.load_tool(
             "get-row-by-id", auth_tokens={"my-test-auth": lambda: auth_token2}
         )
@@ -96,13 +86,13 @@ class TestE2EClient:
         assert "row2" in response["result"]
 
     @pytest.mark.asyncio
-    async def test_run_tool_auth_param_no_auth(self, toolbox, auth_token1):
+    async def test_run_tool_param_auth_no_auth(self, toolbox, auth_token1):
         tool = await toolbox.load_tool("get-row-by-email-auth")
         with pytest.raises(PermissionError, match="Login required"):
             await tool.arun({})
 
     @pytest.mark.asyncio
-    async def test_run_tool_auth_param(self, toolbox, auth_token1):
+    async def test_run_tool_param_auth(self, toolbox, auth_token1):
         tool = await toolbox.load_tool(
             "get-row-by-email-auth", auth_tokens={"my-test-auth": lambda: auth_token1}
         )
