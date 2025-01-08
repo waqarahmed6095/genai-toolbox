8c83100c01cfa71e3e9544113762ccdb6d97f025
Twisha Bansal <twishabansal@google.com>
2025-01-08 17:29:25 +0530

    lint

--- sdks/langchain/tests/conftest.py
+++ sdks/langchain/tests/conftest.py
@@ -25,6 +25,7 @@ import time
 from typing import Generator
 
 import google
+import pytest
 import pytest_asyncio
 from google.auth import compute_engine
 from google.cloud import secretmanager, storage
@@ -141,11 +142,15 @@ def auth_token2(project_id: str) -> str:
 
 @pytest_asyncio.fixture(scope="session")
 def toolbox_server(
-    tmp_path_factory, toolbox_version: str, tools_file_path: str
+    tmp_path_factory: pytest.TempPathFactory,
+    toolbox_version: str,
+    tools_file_path: str
 ) -> Generator:
     """Starts the toolbox server as a subprocess."""
+    # Get a temp dir for toolbox data
     tmp_path = tmp_path_factory.mktemp("toolbox_data")
     toolbox_binary_path = str(tmp_path) + "/toolbox"
+
     # Get toolbox binary
     if toolbox_version == "dev":
         print("Compiling the current dev toolbox version...")
